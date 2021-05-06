7c233a8014d6c70b766fc9f132137277809c085c
singu <singu@DESKTOP-43JAOOR>
2021-05-06 12:18:02 +0900

    두번째 기능추가

--- src/gitTest/HelloWord.java
+++ src/gitTest/HelloWord.java
@@ -5,6 +5,7 @@ public class HelloWord {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Wordl Java !!!");
+		System.out.println("Spring Framework PT");
 
 	}
 
