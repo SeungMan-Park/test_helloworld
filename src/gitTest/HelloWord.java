b03ec9caf8e3ec6d26b818d90c4ada25b5879803
singu <singu@DESKTOP-43JAOOR>
2021-05-06 12:15:42 +0900

    첫번째 작업기능 추가

---
+++ src/gitTest/HelloWord.java
@@ -0,0 +1,11 @@
+package gitTest;
+
+public class HelloWord {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Wordl Java !!!");
+
+	}
+
+}
